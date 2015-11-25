277a1ee1b4282d123a32cd01c2cdab33403fa418
joshitagaur <joshita.gaur@gamil.com>
2015-11-25 10:21:44 +0530

    add program for operator

---
+++ Day-1/operators.js
@@ -0,0 +1,25 @@
+console.log(typeof(4.5))
+VM200:2 number
+undefined
+console.log(NaN=NaN)
+VM293:2 NaN
+undefined
+console.log(NaN==NaN)
+VM354:2 false
+undefined
+8*null
+0
+"5"-1
+4
+"5"+1
+"51"
+"five"*2
+NaN
+false==0
+true
+null==undefined
+true
+null==0
+false
+null || "user"
+"user"
