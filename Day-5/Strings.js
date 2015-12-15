d40e8f1a8d79bd4edea3b69e1e85bc970bd1d424
joshitagaur <joshita.gaur@gamil.com>
2015-12-15 10:40:35 +0530

    add programs for strings

---
+++ Day-5/Strings.js
@@ -0,0 +1,10 @@
+var myString = "Fido"; 
+myString.myProperty = "value"; 
+console.log(myString.myProperty); 
+VM292:4 undefined
+console.log("coconuts".slice(4, 7)); 
+VM293:2 nut
+console.log("coconut".indexOf("u")); 
+VM294:2 5
+console.log(" okay \n ".trim());
+VM295:2 okay
