ba22c0446619300fb01750f24e99635c02006696
joshitagaur <joshita.gaur@gamil.com>
2015-12-30 10:32:41 +0530

    add programs for practise on operators and loops

---
+++ Day-10/practise-on-operators-and-loops.js
@@ -0,0 +1,26 @@
+//program for numbers divisible by 3 and 5 and both
+var i = 1
+for(i; i<=25; i++)
+{
+    if(i%3 == 0 && i%5 != 0)
+    {
+       console.log("fizz")
+    }
+    else if(i%5 == 0 && i%3 != 0)
+    {
+       console.log("buzz")
+    }
+    else if(i%5 == 0 && i%3 == 0)
+    {
+       console.log("fizzbuzz")
+    }
+}
+//->fizz
+ buzz
+ fizz buzz
+ fizz
+ fizzbuzz
+ fizz
+ buzz
+ fizz
+ buzz
