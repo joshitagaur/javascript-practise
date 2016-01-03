256b70e2b820d2a5ae7547d18855fc8ce3f3dd2f
joshitagaur <joshita.gaur@gamil.com>
2016-01-03 23:04:11 +0530

    add programs for practise on operators

---
+++ Day-1/operators.js
@@ -0,0 +1,28 @@
+//Different operators
+console.log(typeof(4.5))
+console.log(NaN=NaN)
+console.log(NaN==NaN)
+//Automatic type conversion
+console.log(8*null)
+console.log("5"-1)
+console.log("5"+1)
+console.log("five"*2)
+//comparing with null and undefined values
+false==0
+null==undefined
+null==0
+null || "user"
+//->number
+
+//->NaN
+
+//->false
+
+//->0
+
+//->4
+//->51
+
+//->NaN
+
+//->"user"
