219e2e3ab27e461b0ab1d8b1783b0e81926cd375
joshitagaur <joshita.gaur@gamil.com>
2016-01-28 11:54:38 +0530

    add programs for closures

---
+++ Day-11/Closure.js
@@ -0,0 +1,14 @@
+//An example of closure in javascript
+function completeName(firstName,lastName)
+{
+   var intro = "Your name is"
+   function showName()
+   {
+      return intro+firstName+" "+lastName
+   }
+   return showName()
+}
+console.log(completeName("Joshita","Gaur"))
+
+//->Your name isJoshita Gaur
+
