51096247e6e5e2ab2535c2540d8a0563f4cbe46a
joshitagaur <joshita.gaur@gamil.com>
2016-03-03 09:36:03 +0530

    add programs for Prototype interference

---
+++ Day-15/Prototype-interference.js
@@ -0,0 +1,31 @@
+//Prototype interference
+var map = {};
+function storeValues(event,value){
+  return map[event] = value;
+}
+storeValues("home","done");
+storeValues("Office","gone");
+Object.prototype.nonesense = "hi";
+for(var i in map)
+{
+  console.log(i);
+}
+//->"home"
+"Office"
+"nonesense"
+
+//Nonesense is not a property in map but it shows up, because it is ennumerable property
+console.log("toString" in map);
+//->true
+//Gives true but doesn't show up in the console since it is nonennumerable property
+
+delete Object.prototype.nonesense;
+//use defineProperty to make properties nonennumerable
+Object.defineProperty(Object.prototype,"nonesense",{ennumerable:false,value:"hi"});
+//check for changes
+for(var i in map)
+{
+  console.log(i);
+}
+//->"home"
+"Office"
